2a46e26a3bbd5b76490f11df6c44ae50c81f8b08
georgfalemark <georg@falemark.se>
2019-02-11 18:52:59 +0100

    Add project files.

---
+++ mercado.nu/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace mercado.nu.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
