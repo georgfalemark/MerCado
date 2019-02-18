44627ab9c5ee9622c96dedd2f5b998146b59b9fe
TobbePersson <spara.energi@gmail.com>
2019-02-18 14:48:47 +0100

    Klasser EF

---
+++ mercado.nu/Models/Responders.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace mercado.nu.Models
+{
+    public class Responders
+    {
+        public Guid PersonId { get; set; }
+        public Person Persons { get; set; }
+
+        public Guid MarketResearchId { get; set; }
+        public MarketResearch MarketResearchs { get; set; }
+    }
+}
