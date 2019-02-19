0b9dc5d34683132f4f804d9447a5a880bca634e7
TobbePersson <spara.energi@gmail.com>
2019-02-19 12:05:48 +0100

    no message

---
+++ mercado.nu/Controllers/QuestionsController.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using mercado.nu.Data;
+using mercado.nu.Models.Entities;
+
+namespace mercado.nu
+{
+    public class QuestionsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public QuestionsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Questions
+        public async Task<IActionResult> Index()
+        {
+            var applicationDbContext = _context.Questions.Include(q => q.Chapter);
+            return View(await applicationDbContext.ToListAsync());
+        }
+
+        // GET: Questions/Details/5
+        public async Task<IActionResult> Details(Guid? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var question = await _context.Questions
+                .Include(q => q.Chapter)
+                .FirstOrDefaultAsync(m => m.QuestionId == id);
+            if (question == null)
+            {
+                return NotFound();
+            }
+
+            return View(question);
+        }
+
+        // GET: Questions/Create
+        public IActionResult Create()
+        {
+            ViewData["ChaptersId"] = new SelectList(_context.Chapters, "ChaptersId", "ChaptersId");
+            return View();
+        }
+
+        // POST: Questions/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("QuestionId,ActualQuestion,ChaptersId")] Question question)
+        {
+            if (ModelState.IsValid)
+            {
+                question.QuestionId = Guid.NewGuid();
+                _context.Add(question);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["ChaptersId"] = new SelectList(_context.Chapters, "ChaptersId", "ChaptersId", question.ChaptersId);
+            return View(question);
+        }
+
+        // GET: Questions/Edit/5
+        public async Task<IActionResult> Edit(Guid? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var question = await _context.Questions.FindAsync(id);
+            if (question == null)
+            {
+                return NotFound();
+            }
+            ViewData["ChaptersId"] = new SelectList(_context.Chapters, "ChaptersId", "ChaptersId", question.ChaptersId);
+            return View(question);
+        }
+
+        // POST: Questions/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(Guid id, [Bind("QuestionId,ActualQuestion,ChaptersId")] Question question)
+        {
+            if (id != question.QuestionId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(question);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!QuestionExists(question.QuestionId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["ChaptersId"] = new SelectList(_context.Chapters, "ChaptersId", "ChaptersId", question.ChaptersId);
+            return View(question);
+        }
+
+        // GET: Questions/Delete/5
+        public async Task<IActionResult> Delete(Guid? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var question = await _context.Questions
+                .Include(q => q.Chapter)
+                .FirstOrDefaultAsync(m => m.QuestionId == id);
+            if (question == null)
+            {
+                return NotFound();
+            }
+
+            return View(question);
+        }
+
+        // POST: Questions/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(Guid id)
+        {
+            var question = await _context.Questions.FindAsync(id);
+            _context.Questions.Remove(question);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool QuestionExists(Guid id)
+        {
+            return _context.Questions.Any(e => e.QuestionId == id);
+        }
+    }
+}
