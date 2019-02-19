4f197a02c6b968005af9eb91dc2f1dd6a91133d6
TobbePersson <spara.energi@gmail.com>
2019-02-19 14:36:41 +0100

    Tobias ViewModels

---
+++ mercado.nu/Data/DataAccessQuestions.cs
@@ -0,0 +1,25 @@
+﻿using mercado.nu.Models.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace mercado.nu.Data
+{
+    public class DataAccessQuestions
+    {
+        public ApplicationDbContext _questionContext;
+
+        public DataAccessQuestions(ApplicationDbContext questionContext)
+        {
+            _questionContext = questionContext;
+        }
+
+        internal async Task SaveChapter(Chapters chapters)
+        {
+            chapters.ChaptersId = Guid.NewGuid();
+            _questionContext.Add(chapters);
+            await _questionContext.SaveChangesAsync();
+        }
+    }
+}
