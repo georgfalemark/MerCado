7a581a2807818f7072a7f1e05473dfd1b4bc9764
georgfalemark <georg@falemark.se>
2019-02-20 12:15:09 +0100

    inloggningsfunktion funkar inte!

--- mercado.nu/Areas/Identity/Pages/Account/Login.cshtml.cs
+++ mercado.nu/Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -18,11 +18,14 @@ namespace mercado.nu.Areas.Identity.Pages.Account
     {
         private readonly SignInManager<ApplicationUser> _signInManager;
         private readonly ILogger<LoginModel> _logger;
+        private readonly UserManager<ApplicationUser> _userManager;
 
-        public LoginModel(SignInManager<ApplicationUser> signInManager, ILogger<LoginModel> logger)
+
+        public LoginModel(SignInManager<ApplicationUser> signInManager, ILogger<LoginModel> logger, UserManager<ApplicationUser> userManager)
         {
             _signInManager = signInManager;
             _logger = logger;
+            _userManager = userManager;
         }
 
         [BindProperty]
@@ -74,7 +77,12 @@ namespace mercado.nu.Areas.Identity.Pages.Account
             {
                 // This doesn't count login failures towards account lockout
                 // To enable password failures to trigger account lockout, set lockoutOnFailure: true
+
+                //ApplicationUser signedUser = await _userManager.FindByEmailAsync(Input.Email);
+                //var result = await _signInManager.PasswordSignInAsync(signedUser.UserName, Input.Password, Input.RememberMe, lockoutOnFailure: false);
                 var result = await _signInManager.PasswordSignInAsync(Input.Email, Input.Password, Input.RememberMe, lockoutOnFailure: true);
+
+
                 if (result.Succeeded)
                 {
                     _logger.LogInformation("User logged in.");
--- mercado.nu/Startup.cs
+++ mercado.nu/Startup.cs
@@ -38,7 +38,7 @@ namespace mercado.nu
                 options.MinimumSameSitePolicy = SameSiteMode.None;
             });
 
-            
+
             services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseSqlServer(
                     Configuration.GetConnectionString("DefaultConnection")));
