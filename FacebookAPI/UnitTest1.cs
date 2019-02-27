703badfd915588d7591214b4347b824a1e5b0552
georgfalemark <georg@falemark.se>
2019-02-27 18:39:52 +0100

    login sociala medier

--- FacebookAPI/UnitTest1.cs
+++ FacebookAPI/UnitTest1.cs
@@ -6,8 +6,17 @@ namespace FacebookAPI
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public void Get_Info_From_Facebook_User()
         {
+            FacebookService facebookService = new FacebookService();
+
+
+            //Här snappar vi VEM det är!
+
+
+
+
+            var result = facebookService.GetInfoFromFaceBookUser("Nån jäkla personnyckel");//.Result;
         }
     }
 }
