903c971719f3e7787c19271387189063af7c8db3
TobbePersson <spara.energi@gmail.com>
2019-02-27 15:50:59 +0100

    no message

---
+++ mercado.nu/ContentsController.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using mercado.nu.Data;
+using mercado.nu.Models.Entities;
+
+namespace mercado.nu
+{
+    public class ContentsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ContentsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Contents
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Contents.ToListAsync());
+        }
+
+        // GET: Contents/Details/5
+        public async Task<IActionResult> Details(Guid? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var content = await _context.Contents
+                .FirstOrDefaultAsync(m => m.ContentId == id);
+            if (content == null)
+            {
+                return NotFound();
+            }
+
+            return View(content);
+        }
+
+        // GET: Contents/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Contents/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("ContentId,Headline,Body,Created,Publish")] Content content)
+        {
+            if (ModelState.IsValid)
+            {
+                content.ContentId = Guid.NewGuid();
+                _context.Add(content);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(content);
+        }
+
+        // GET: Contents/Edit/5
+        public async Task<IActionResult> Edit(Guid? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var content = await _context.Contents.FindAsync(id);
+            if (content == null)
+            {
+                return NotFound();
+            }
+            return View(content);
+        }
+
+        // POST: Contents/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(Guid id, [Bind("ContentId,Headline,Body,Created,Publish")] Content content)
+        {
+            if (id != content.ContentId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(content);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ContentExists(content.ContentId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(content);
+        }
+
+        // GET: Contents/Delete/5
+        public async Task<IActionResult> Delete(Guid? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var content = await _context.Contents
+                .FirstOrDefaultAsync(m => m.ContentId == id);
+            if (content == null)
+            {
+                return NotFound();
+            }
+
+            return View(content);
+        }
+
+        // POST: Contents/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(Guid id)
+        {
+            var content = await _context.Contents.FindAsync(id);
+            _context.Contents.Remove(content);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ContentExists(Guid id)
+        {
+            return _context.Contents.Any(e => e.ContentId == id);
+        }
+    }
+}
