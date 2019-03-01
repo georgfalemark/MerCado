95899662ecc714581958bcc436e5242d92f37083
TobbePersson <spara.energi@gmail.com>
2019-03-01 13:03:45 +0100

    no message

--- mercado.nu/Startup.cs
+++ mercado.nu/Startup.cs
@@ -76,7 +76,7 @@ namespace mercado.nu
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-            if (env.IsDevelopment())
+            if (true) //(env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
